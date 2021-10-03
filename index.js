fb5a45d8488ebb4fe19688b302b97720db10d03b
Benjamin <ben.d.maryland@gmail.com>
2021-10-03 17:05:55 -0400

    commit tree 0b0d15fb3c45af6f74563ba5481e83f0bc5d4e2f

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code here!
+
+const element = document.createElement("div");
+document.body.append(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+ element.append(ul);
+ 
+
+
+
+
+  console.log(document.querySelector("main"))
+   document.querySelector("main").remove()
+  console.log(document.querySelector("main"))
+  const main = document.getElementById("main");
+ 
+    const element2 = document.createElement("h1");
+document.body.append(element2);
+
+
+function newHeader (name)    {
+  const newHeader1 = document.querySelector('h1')
+
+newHeader1.id="victory"
+  document.body.append(newHeader1)
+  newHeader1.textContent  = `${name} the champion `
+console.log(newHeader1)
+}
+newHeader("ben")
